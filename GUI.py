ba02036804524924617a50bf264aa24af2dcfe6b
Thiago <thijalves@gmail.com>
2023-03-27 21:22:41 -0300

    fix PIL conversion problems

--- GUI.py
+++ GUI.py
@@ -23,10 +23,10 @@ class App:
         self.slider_frame = tk.Frame(self.window, width=600, height=50, bd=1, relief="solid")
         self.slider_frame.grid(row=0, column=1, pady=10, padx=20)
         #hue sliders
-        self.hue_min_slider = tk.Scale(self.slider_frame, label='HUE Min', from_=0, to=360, orient=tk.HORIZONTAL)
+        self.hue_min_slider = tk.Scale(self.slider_frame, label='HUE Min', from_=0, to=180, orient=tk.HORIZONTAL)
         self.hue_min_slider.grid(row=0, column=0, pady=10, padx=20)
-        self.hue_max_slider = tk.Scale(self.slider_frame, label='HUE Max', from_=0, to=360, orient=tk.HORIZONTAL)
-        self.hue_max_slider.set(360)
+        self.hue_max_slider = tk.Scale(self.slider_frame, label='HUE Max', from_=0, to=180, orient=tk.HORIZONTAL)
+        self.hue_max_slider.set(180)
         self.hue_max_slider.grid(row=0, column=1, pady=10, padx=20)
         #saturation sliders
         self.saturation_min_slider = tk.Scale(self.slider_frame, label='Saturation Min', from_=0, to=255, orient=tk.HORIZONTAL)
@@ -53,42 +53,49 @@ class App:
 
         #open source
         self.video = cv2.VideoCapture(source)
+        # self.video.set(cv2.CAP_PROP_FRAME_WIDTH, 400)
+        # self.video.set(cv2.CAP_PROP_FRAME_HEIGHT, 400)
 
         # start the video display loop
         self.update()
 
     def update(self):
-        # get a frame from the video source
+        # Definir range
+        lower_range = np.array([self.hue_min_slider.get(),self.saturation_min_slider.get(),self.value_min_slider.get()])
+        upper_range = np.array([self.hue_max_slider.get(),self.saturation_max_slider.get(),self.value_max_slider.get()])
+        
         ret, frame = self.video.read()
+    
         if ret:
-            # convert the frame to a PIL image
+            # cv2.imshow('Original', frame)
+            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            # cv2.imshow('hsv', hsv)
+            mask = cv2.inRange(hsv, lower_range, upper_range)
+            # cv2.imshow('mascara', mask)
+            result = cv2.bitwise_and(frame, frame, mask=mask)
+            
+            # convert the original frame to a PIL image
             img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             imgArray = Image.fromarray(img)
             img_tk = ImageTk.PhotoImage(imgArray)
-            # update the original canvas with the new image
+            # update the original frame canvas
             self.window.originalCanvas.create_image(0, 0, anchor=tk.NW, image=img_tk)
             self.window.originalCanvas.img_tk = img_tk
 
-            # Definir range
-            lower_range = np.array([self.hue_min_slider.get(),self.saturation_min_slider.get(),self.value_min_slider.get()])
-            upper_range = np.array([self.hue_max_slider.get(),self.saturation_max_slider.get(),self.value_max_slider.get()])
-
-            #update the mask canvas
-            mask = cv2.inRange(frame, lower_range, upper_range)
-            result = cv2.bitwise_and(frame, frame, mask=mask)
-            mask = cv2.cvtColor(mask, cv2.COLOR_BGR2RGB)
+            # convert the mask frame to a PIL image
             maskArray = Image.fromarray(mask)
-            mask_tk = ImageTk.PhotoImage(maskArray)
-            # update the mask canvas with the new image
-            self.window.maskCanvas.create_image(0, 0, anchor=tk.NW, image=mask_tk)
-            self.window.maskCanvas.img_tk = mask_tk
+            array_tk = ImageTk.PhotoImage(maskArray)
+            # update the mask frame canvas
+            self.window.maskCanvas.create_image(0, 0, anchor=tk.NW, image=array_tk)
+            self.window.maskCanvas.img_tk = array_tk
 
-            #update the result canvas
-            result = cv2.cvtColor(result, cv2.COLOR_BGR2RGB)
-            resultArray = Image.fromarray(result)
-            result_tk = ImageTk.PhotoImage(resultArray)
-            self.window.resultCanvas.create_image(0, 0, anchor=tk.NW, image=result_tk)
-            self.window.resultCanvas.img_tk = result_tk
+            # convert the result frame to a PIL image
+            img = cv2.cvtColor(result, cv2.COLOR_BGR2RGB)
+            resultArray = Image.fromarray(img)
+            array_tk = ImageTk.PhotoImage(resultArray)
+            # update the result frame canvas
+            self.window.resultCanvas.create_image(0, 0, anchor=tk.NW, image=array_tk)
+            self.window.resultCanvas.img_tk = array_tk
 
         # call this function again after 15 milliseconds
         self.window.after(15, self.update)
